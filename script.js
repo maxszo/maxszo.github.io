7713cc2e1c29c18640a5c06936cf6e3c00b84173
Max Szostkiewicz <max.szo123@gmail.com>
2025-06-27 00:58:24 +0200

    feat: add more fake content and beautify

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// Pour du JS simple (futur interactivité éventuelle)
-console.log("Bienvenue sur mon portfolio !");
+// Animations avec ScrollReveal (lib externe)
+ScrollReveal({
+  distance: '40px',
+  duration: 1000,
+  easing: 'ease-out',
+  origin: 'bottom',
+  reset: false
+});
+
+ScrollReveal().reveal('.section-title', { delay: 200 });
+ScrollReveal().reveal('.section-content, .project, .timeline li', {
+  delay: 400,
+  interval: 100
+});
